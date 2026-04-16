d4a7ff5b83c842b4879c70960aa08234a2246066
ARTHUR GONÇALVES POLO <25.00376-5@maua.br>
2026-04-15 21:58:33 -0300

    ArraysMetodos

--- ads1002/colecoes/OperacoesVetores.java
+++ ads1002/colecoes/OperacoesVetores.java
@@ -5,11 +5,13 @@ public class OperacoesVetores {
     static int[] num = new int[5];
     static int quantidade = 0; // meu index(i)
     static String menu = "1 - Adicionar\n 2 - Exibir\n 3 - Buscar\n 4 - Ver o maior\n 0 - Sair";
+
     public static void main(String[] args) {
         int opcao;
         do {
             // input para o usuário
-            opcao = Integer.parseInt(JOptionPane.showInputDialog(menu)); // retorna string se necessário converter tipo de variável
+            opcao = Integer.parseInt(JOptionPane.showInputDialog(menu)); // retorna string se necessário converter tipo
+                                                                         // de variável
             switch (opcao) {
                 case 1:
                     adicionar();
@@ -26,6 +28,7 @@ public class OperacoesVetores {
             }
         } while (opcao != 0);
     }
+
     static void adicionar() {
         if (quantidade == num.length) {
             JOptionPane.showMessageDialog(null, "Vetor cheio");
@@ -36,7 +39,7 @@ public class OperacoesVetores {
         quantidade++;
         JOptionPane.showMessageDialog(null, "Valor adicionado com sucesso");
     }
-    
+
     static void exibir() {
         if (quantidade == 0) {
             JOptionPane.showMessageDialog(null, "Valor vazio");
@@ -50,10 +53,31 @@ public class OperacoesVetores {
     }
 
     static void buscar() {
-
+        // Se estiver vazio avisa e termina caso o contrário pega o valor e retorna se
+        // existir
+        if (quantidade == 0) {
+            JOptionPane.showMessageDialog(null, "Nenhum valor cadastrado");
+            return;
+        }
+        int valor = Integer.parseInt(JOptionPane.showInputDialog("Qual valor deseja buscar"));
+        for (int i = 0; i < quantidade; i++) {
+            if (valor == num[i]) {
+                JOptionPane.showMessageDialog(null, "Encontrado na posição: " + i);
+                return;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Elemento não encontrado");
     }
 
     static void maior() {
-
+        if (quantidade == 0) {
+            JOptionPane.showMessageDialog(null, "Nenhum valor cadastrado");
+            return;
+        }
+        int maior = num[0];
+        for (int i = 1; i < quantidade; i++) {
+            maior = num[i] > maior ? num[i] : maior; // if-else de uma linha só 
+        }
+        JOptionPane.showMessageDialog(null, maior + " é o maior valor");
     }
 }
